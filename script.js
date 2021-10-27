0b1c34d5ce88258c3a19f63a8cdf52d5d40e0ea9
rocky <rockpower666fire@gmail.com>
2021-10-27 19:41:46 +0530

    adding starting project

---
+++ script.js
@@ -0,0 +1,32 @@
+window.onload = () => {
+  document.querySelector('#calc').onclick = calculateTip;
+}
+function calculateTip() {
+  let amount = document.querySelector('#inp1').value;
+  let tip = document.querySelector('button').value;
+  let persons = document.querySelector('#inp2').value;
+  console.log(tip)
+  if (amount === "") {
+    alert('HaramZada Bill Na deiki kn basipadichu');
+    return;
+  }
+  if (persons === "1") {
+    document.querySelector('#rupees1').style.display = "none";
+  }
+  else {
+    document.querySelector('#rupees1').style.display = 'block';
+  }
+  let total = (amount * tip) / persons;
+  console.log(total)
+  total = Math.round(total * 100) / 100;
+  total = total.toFixed(2);
+  console.log(total);
+  document.querySelector('#rupees1').style.display = 'block';
+  document.querySelector('#rupees1').innerHTML = total;
+
+  let tp = amount / persons;
+  tp = tp.toFixed(2);
+  console.log(tp)
+  // document.querySelector('#rupees2').style.display = 'block';
+  document.querySelector('#rupees2').innerHTML = tp;
+}
